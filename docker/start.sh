30290d00d3a5b00d7120b10452ab501ffd4b36c6
Ali-M-Zaitoun <alibnmohammad92@gmail.com>
2026-04-16 06:10:23 +0300

    Update Docker

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Waiting for database connection..."
-until php artisan db:monitor --databases=mysql 2>/dev/null; do
+until php artisan db:monitor --databases=pqsql 2>/dev/null; do
     echo "DB not ready, retrying in 3 seconds..."
     sleep 3
 done
@@ -11,4 +11,4 @@ php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 
-php artisan serve --host=0.0.0.0 --port=8080
+php artisan serve --host=0.0.0.0 --port=$PORT
